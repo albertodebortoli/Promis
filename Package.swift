b1dd6541b1db62b3f80760c748f4ebddf09f047c
Alberto De Bortoli <albertodebortoli@gmail.com>
2022-06-06 23:29:59 +0100

    Add SPM support

--- Package.swift
+++ Package.swift
@@ -1,34 +1,22 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.5
 
 import PackageDescription
 
 let package = Package(
     name: "Promis",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Promis",
-            targets: ["Promis"]),
-    ],
-    dependencies: [
-        // Here we define our package's external dependencies
-        // and from where they can be fetched:
-        .package(
-            url: "https://github.com/apple/swift-argument-parser.git",
-            from: "1.1.1"
-        )
+            targets: ["Promis"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Promis",
-            dependencies: ["ArgumentParser"],
-            path: "./Promis/Classes/"),
+            path: "Framework/Sources"),
         .testTarget(
             name: "PromisTests",
             dependencies: ["Promis"],
-            path: "./Example/Tests/"),
+            path: "Tests/Sources")
     ]
 )
